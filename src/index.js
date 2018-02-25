ac9059d1cfb8cecee2722428ed738cb520a881ec
19alexandr90 <bestsanfirst@gmail.com>
2018-02-25 14:35:50 +0300

    love-triangle

--- src/index.js
+++ src/index.js
@@ -3,18 +3,25 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  let count = 0;
-  for (i = 0; i = preferences.length - 1; i++) {
-      let index1 = i;                         
-      let number1 = preferences[index1];           
-      let index2 = number1 - 1;               
-      let number2 = preferences[index2];            
-      let index3 = number2 - 1;               
-      let number3 = preferences[index3];            // 1
-      let indexFinal = number3 - 1;           // 1 - 1 = 0 (-1 из-за программистов)
-      if (indexFinal === index1) {
-          count++;                            // count = count + 1;  
-      };
-  }
-  return count / 3;
-};
+    let count = 0;
+    for (let i = 0; i < preferences.length; i++) {
+      let index1 = i;
+      if (preferences[index1] && preferences[index1] !== i + 1) {
+        let number1 = preferences[index1];
+        let index2 = number1 - 1;
+        if (preferences[index2]) {
+          let number2 = preferences[index2];
+          let index3 = number2 - 1;
+          if (preferences[index3]) {
+            let number3 = preferences[index3];
+            let indexFinal = number3 - 1;
+            if (indexFinal === index1) {
+              count++;
+            };
+          }
+        }
+      }
+    }
+    return (count  / 3);
+  };
+  
